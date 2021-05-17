d580a0930f2f4d6628074e4a7f243fde32a3adc7
Theo wald <theo83wald@gmail.com>
2021-05-17 14:50:04 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'region',
+        component: PorRegionComponent
+    },
+    {
+        path: 'capital',
+        component: PorCapitalComponent
+    },
+    {
+        path: 'pais/:id',
+        component: VerPaisComponent
+    },
+    {
+        //Por si hay otras rutas, o se intenta llegar para alla
+        //que vaya a home (aunque podria ir a un componente)
+        path: '**',
+        redirectTo: ''
+    }
+];
+
+@NgModule({
+    exports: [
+        RouterModule
+    ],
+    imports: [
+        RouterModule.forRoot(routes)
+    ]
+})
+export class AppRoutingModule {}
