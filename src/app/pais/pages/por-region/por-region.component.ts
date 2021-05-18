b8f40327e26439c5fe2b9358db141fbf9ac6141d
Theo wald <theo83wald@gmail.com>
2021-05-18 17:49:29 +0200

    fin 18_05_2021

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -5,29 +5,35 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`
+      button {
+        margin-right: 5px;
+      }
+  `]
 })
 export class PorRegionComponent {
 
-  termino: string = '';
-  hayError: boolean = false;
   paises: Country[] = [];
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
   constructor(private paisService: PaisService) { }
 
-  buscar(termino: string){
-    this.hayError = false;
-    this.termino = termino;
-    //console.log(this.termino);
+  getClaseCSS( region: string ): string{
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary'
+  }
 
-    this.paisService.buscarRegion(this.termino)
+  activarRegion (region: string){
+
+    if ( region === this.regionActiva ){ return; }
+
+    this.regionActiva = region;
+    this.paises = [];     
+    this.paisService.buscarRegion(this.regionActiva)
       .subscribe( (res) => {
-        console.log(res);
         this.paises = res;
       }, (err) => {
-        this.paises = [];
-        this.hayError = true;        
+        this.paises = [];      
       });
   }
 
